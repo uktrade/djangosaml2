35d85f332dd87e036177f35865b7cdab94984de4
peppelinux <giuseppe.demarco@unical.it>
2021-06-20 23:42:35 +0200

    v1.3.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 
 setup(
     name='djangosaml2',
-    version='1.3.0',
+    version='1.3.2',
     description='pysaml2 integration for Django',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
