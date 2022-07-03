27e273373d0f6ca9cd1a15a44729dc2d2e65e596
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2022-07-03 16:22:37 +0200

    bumped version to 1.5.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 setup(
     name="djangosaml2",
-    version="1.5.0",
+    version="1.5.1",
     description="pysaml2 integration for Django",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
