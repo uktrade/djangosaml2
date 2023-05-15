2366a927fa9bb36d1ee49009eda2513d553e7896
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-05-15 12:15:41 +0100

    Bump package version to 1.5.8

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 setup(
     name="djangosaml2",
-    version="1.5.7",
+    version="1.5.8",
     description="pysaml2 integration for Django",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
